e43d8c6da09841c1b3c917bc47379df06818fe95
Dominik Horn <dominik.horn@tum.de>
2021-03-29 16:34:36 +0200

    use list comprehensions for exp()

--- benchmark/generated/exp.py
+++ benchmark/generated/exp.py
@@ -1,5 +1,2 @@
 def exp(base: int, exp_min: int, exp_max: int, exp_step: int):
-    l = []
-    for i in list(range(exp_min, exp_max + 1, exp_step)):
-        l.append(pow(base, i))
-    return l
+    return [pow(base, i) for i in list(range(exp_min, exp_max + 1, exp_step))]
